b14edebda8f00e3e1cbed88fc3cfa76a0533c357
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-06-02 23:04:27 -0700

    Initial Commit

---
+++ FeelsLike+/FeelsLike_App.swift
@@ -0,0 +1,17 @@
+//
+//  FeelsLike_App.swift
+//  FeelsLike+
+//
+//  Created by Ikbal Demirdoven on 2023-06-02.
+//
+
+import SwiftUI
+
+@main
+struct FeelsLike_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
